3f08bc5616e0a63dfed1d701bec60fd288b8c883
rafay-484 <rafayahmad484@gmail.com>
2026-03-03 13:02:55 +0500

    Initial commit: DevSecOps Workshop application

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('DevSecOps Workshop Working!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
